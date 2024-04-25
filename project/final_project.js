95c9941eb04d381f030f5330a6be9ffafed88c66
jeremymecham <a02286624@aggies.usu.edu>
2024-04-24 19:10:10 -0600

    Final Project
    Removed useless JS code

--- project/final_project.js
+++ project/final_project.js
@@ -41,20 +41,3 @@ photos.forEach(function(photo) {
     photo.addEventListener('click', enlargePhoto);
 });
 
-function loadHighQualityImages() {
-    var images = document.querySelectorAll('.photo img');
-    images.forEach(function(img) {
-        var highQualityUrl = img.dataset.src;
-        var highQualityImg = new Image();
-        highQualityImg.src = highQualityUrl;
-        highQualityImg.onload = function() {
-            // Replace placeholder with high-quality image
-            img.src = highQualityUrl;
-        };
-    });
-}
-
-// Load high-quality images when the DOM content is loaded
-document.addEventListener("DOMContentLoaded", function() {
-    loadHighQualityImages();
-});
